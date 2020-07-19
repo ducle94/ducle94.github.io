3c2e2581e0ec32354370c3662f5147a9ae0e465c
Duc Le <hieuduc194@gmail.com>
2020-07-20 00:28:51 +0300

    Updates

---
+++ precache-manifest.dcdd4d1e597dbec74b6e91a6001b84fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e1f109ade52836299adf32f3cafa33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca202a74dc655cd706a2",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "df8fc2fb7c7effb1c600",
+    "url": "/static/js/2.5a08db1e.chunk.js"
+  },
+  {
+    "revision": "ca202a74dc655cd706a2",
+    "url": "/static/js/main.61dd6e07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d0a00386d444f706f0f94fe03e8ce1d",
+    "url": "/static/media/ConnectWithFinland.6d0a0038.jpg"
+  },
+  {
+    "revision": "ca1ea78ef87a13b4db129a9c2c8c9b54",
+    "url": "/static/media/profile.ca1ea78e.jpg"
+  },
+  {
+    "revision": "7833dcd18fbd8b6556ee4875b581f2ad",
+    "url": "/static/media/soulution2019.7833dcd1.jpg"
+  }
+]);
